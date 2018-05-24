d91ea211de9b529699fee6dd278de898c2c6342e
marcus02 <marcus02@opensource.gov>
2018-05-24 15:16:47 -0400

    Minor changes

--- LinkedList/src/com/group305/LinkedList/LinkedList.java
+++ LinkedList/src/com/group305/LinkedList/LinkedList.java
@@ -16,8 +16,40 @@ public class LinkedList {
 	
 	public LinkedList() {
 		// Create the head to initialize the list.
-		this.head = null;
-		this.length = 0;
+		this.setHead(null);
+		this.setLength(0);
+	}
+
+	public void add(Object data) {
+		
+	}
+	
+	/**
+	 * @return the head
+	 */
+	public Node getHead() {
+		return head;
+	}
+
+	/**
+	 * @param head the head to set
+	 */
+	public void setHead(Node head) {
+		this.head = head;
+	}
+
+	/**
+	 * @return the length
+	 */
+	public int getLength() {
+		return length;
+	}
+
+	/**
+	 * @param length the length to set
+	 */
+	public void setLength(int length) {
+		this.length = length;
 	}
 	
 	
